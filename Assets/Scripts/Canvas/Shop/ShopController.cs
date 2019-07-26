217231706632554143f6a9c494f818c075c7e89e
SteelMan <concetto.pollicina@gmail.com>
2019-07-26 22:30:25 +0200

    feat(shop): add shop and powerup logic

---
+++ Assets/Scripts/Canvas/Shop/ShopController.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[Serializable]
+public class ShopController : MonoBehaviour
+{
+    private const int BASE_POWERUP_COST = 5000;
+    private enum Property {
+        Health,
+        Attack
+    }
+    private StorageSpaceship[] spaceships;
+    private int currentIndex;
+
+    public Button next;
+    public Button prev;
+    public RectTransform spaceshipImage;
+    public Text spaceshipName;
+
+    public Text healthPrice;
+    public Button boostHealth;
+    public Text attackPrice;
+    public Button boostAttack;
+
+    public RectTransform healthIndicatorContainer;
+    public RectTransform attackIndicatorContainer;
+    public GameObject powerUpIndicator;
+
+    private int calcPowerupCost(int level)
+    {
+        return (int) Mathf.Pow(2, level) * BASE_POWERUP_COST;
+    }
+
+    private void UpdateView()
+    {
+        StorageSpaceship spaceship = this.spaceships[this.currentIndex];
+
+        this.DisableButtons();
+
+        this.spaceshipName.text = spaceship.name;
+        this.healthPrice.text = this.calcPowerupCost(spaceship.healthPowerUpsCount).ToString("N0");
+        this.attackPrice.text = this.calcPowerupCost(spaceship.attackPowerUpsCount).ToString("N0");
+
+        this.FillPowerUpIndicator(
+            this.healthIndicatorContainer,
+            spaceship.healthPowerUpsCount,
+            spaceship.healthPowerUpsMax
+        );
+        this.FillPowerUpIndicator(
+            this.attackIndicatorContainer,
+            spaceship.attackPowerUpsCount,
+            spaceship.attackPowerUpsMax
+        );
+    }
+
+    private void FillPowerUpIndicator(RectTransform spawnPoint, int active, int total)
+    {
+        for (int i = 0; i < total; ++i) {
+            float spawnX = i * (5 + 1);
+            Vector3 pos = new Vector3(spawnX, 0, 0);
+
+            GameObject spawnedItem = Instantiate(this.powerUpIndicator);
+            spawnedItem.transform.position = pos;
+            spawnedItem.transform.SetParent(spawnPoint, false);
+
+            if (i > active) {
+                spawnedItem.GetComponent<Image>().color = new Color(200, 200, 200);
+            }
+        }
+    }
+
+    private void DisableButtons()
+    {
+        StorageSpaceship spaceship = this.spaceships[this.currentIndex];
+
+        this.next.enabled = this.currentIndex != this.spaceships.Length - 1;
+        this.prev.enabled = this.currentIndex != 0;
+        this.boostHealth.enabled = spaceship.healthPowerUpsCount != spaceship.healthPowerUpsMax;
+        this.boostAttack.enabled = spaceship.attackPowerUpsCount != spaceship.attackPowerUpsMax;
+    }
+
+    private void OnNextClick()
+    {
+        ++this.currentIndex;
+        this.UpdateView();
+    }
+
+    private void OnPrevClick()
+    {
+        --this.currentIndex;
+        this.UpdateView();
+    }
+
+    private void OnBoostClick(Property p)
+    {
+        StorageSpaceship spaceship = this.spaceships[this.currentIndex];
+
+        switch (p) {
+            case Property.Health:
+                ++spaceship.healthPowerUpsCount;
+                break;
+            case Property.Attack:
+                ++spaceship.attackPowerUpsCount;
+                break;
+            default:
+                throw new System.Exception("Error: Mising property");
+        }
+
+        StorageSpaceship.Write(spaceship);
+        this.UpdateView();
+    }
+
+    void Start()
+    {
+        this.spaceships = StorageSpaceship.GetAll();
+        this.currentIndex = 0;
+
+        this.next.onClick.AddListener(delegate {OnNextClick(); });
+        this.prev.onClick.AddListener(delegate {OnPrevClick(); });
+
+        this.boostHealth.onClick.AddListener(delegate {OnBoostClick(Property.Health); });
+        this.boostAttack.onClick.AddListener(delegate {OnBoostClick(Property.Attack); });
+
+        this.UpdateView();
+    }
+
+}
---
+++ Assets/Scripts/Model/StorageSpaceship.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class StorageSpaceship
+{
+    public string name;
+    public int healthPowerUpsCount;
+    public int attackPowerUpsCount;
+    public int healthPowerUpsMax = 10;
+    public int attackPowerUpsMax = 10;
+
+    public StorageSpaceship(
+        string name,
+        int healthPowerUpsCount,
+        int attackPowerUpsCount
+    ) {
+        this.name = name;
+        this.healthPowerUpsCount = healthPowerUpsCount;
+        this.attackPowerUpsCount = attackPowerUpsCount;
+    }
+
+    private static StorageSpaceship[] Load()
+    {
+        string[] spaceshipTypes = (string[]) Enum.GetNames(typeof(SpaceshipType));
+        StorageSpaceship[] spaceships = new StorageSpaceship[spaceshipTypes.Length];
+
+        for (int i = 0; i < spaceshipTypes.Length; ++i) {
+            spaceships[i] = new StorageSpaceship(
+                spaceshipTypes[i],
+                0,
+                0
+            );
+        }
+
+        return spaceships;
+    }
+
+    public static void Write(StorageSpaceship spaceship)
+    {
+        StorageSpaceship[] spaceships = StorageSpaceship.GetAll();
+
+        // search element in spaceships
+        for (int i = 0; i < spaceships.Length; ++i) {
+            if (spaceships[i].name == spaceship.name) {
+                spaceships[i] = spaceship;
+                break;
+            }
+        }
+
+        // replace element in spaceships
+        string json = JsonHelper.ToJson(spaceships, true);
+
+        PlayerPrefs.SetString("Spaceships", json);
+        PlayerPrefs.Save();
+    }
+
+    public static StorageSpaceship[] GetAll()
+    {
+        string spaceshipsString = PlayerPrefs.GetString("Spaceships", "");
+
+        if (spaceshipsString == "") {
+            return Load();
+        }
+
+        return JsonHelper.FromJson<StorageSpaceship>(spaceshipsString);
+    }
+}
