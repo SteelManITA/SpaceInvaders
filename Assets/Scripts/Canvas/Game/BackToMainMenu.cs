460c085a073dd5617d24cd5b70a221201016a6d1
SteelMan <concetto.pollicina@gmail.com>
2019-07-28 10:00:18 +0200

    feat(game): add end game button

---
+++ Assets/Scripts/Canvas/Game/BackToMainMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BackToMainMenu : MonoBehaviour
+{
+    private Button button;
+
+    void Start()
+    {
+        this.button = GetComponent<Button>();
+        this.button.onClick.AddListener(delegate {OnButtonClick(); });
+    }
+
+    void OnButtonClick()
+    {
+        IPlayer player = GameState.getInstance().getPlayer();
+        player.hurt(player.getTotalHealth());
+    }
+}
