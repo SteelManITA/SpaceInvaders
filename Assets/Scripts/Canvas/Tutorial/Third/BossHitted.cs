45135e95cb8976ab181e977595daa01cef552224
SteelMan <concetto.pollicina@gmail.com>
2019-07-28 13:45:15 +0200

    feat(tutorial): add boss screen to tutorial

---
+++ Assets/Scripts/Canvas/Tutorial/Third/BossHitted.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossHitted : MonoBehaviour
+{
+    public GameObject shield;
+    int hitCount = 0;
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag == "BulletPlayer") {
+            ++hitCount;
+            if (hitCount%3 != 0) {
+                return;
+            }
+            
+            Vector3 shieldOffset = new Vector3(0, -1.5f, 0);
+            GameObject shield = Instantiate(
+                this.shield,
+                GetComponent<Transform>().position + shieldOffset,
+                Quaternion.identity,
+                this.GetComponent<Transform>().parent
+            );
+            ShieldController sh = shield.AddComponent<ShieldController>();
+            sh.owner = GetComponent<Transform>();
+            sh.shieldOffset = shieldOffset;
+        }
+    }
+}
---
+++ Assets/Scripts/Canvas/Tutorial/Third/BulletHitBoss.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletHitBoss : MonoBehaviour
+{
+    public GameObject model;
+
+    void Awake()
+    {
+        GetComponent<Renderer>().material.color = Color.green;
+        BulletController controller = GetComponent<BulletController>();
+        controller.setSpeed(-0.3f);
+        controller.tag = "BulletPlayer";
+    }
+
+    void Respawn()
+    {
+        GameObject go = Instantiate(
+            this.model,
+            new Vector3(0, -7f, 0),
+            Quaternion.identity,
+            this.GetComponent<Transform>().parent
+        );
+        BulletHitBoss hitBoss = go.AddComponent<BulletHitBoss>();
+        hitBoss.model = this.model;
+    }
+    
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag == "Enemy" || other.tag == "Shield") {
+            this.Respawn();
+            Destroy(this.gameObject);
+        }
+    }
+}
